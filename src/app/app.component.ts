246cad5c99564591fc34abef3f0bf67ec9b450ed
Ваше Имя <you@example.com>
2021-03-04 17:15:15 +0200

    clear-project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import {Observable} from "rxjs";
-import {UpdateOn} from "./store/actions/counter.actions";
-import {Store} from "@ngrx/store";
 
 
 @Component({
@@ -10,30 +7,5 @@ import {Store} from "@ngrx/store";
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public count$: Observable<number>;
-  private inpValue: number = 0;
 
-  constructor(private store: Store<{ count: number }>) {
-    this.count$ = this.store.select('count');
-  }
-
-  increase() {
-    // this.store.dispatch();
-  }
-
-  decrease() {
-    // this.store.dispatch(decrement());
-  }
-
-  clear() {
-    // this.store.dispatch(reset());
-  }
-
-  updateOn() {
-    this.store.dispatch(new UpdateOn(this.inpValue));
-  }
-
-  onInpBlur(event: any) {
-    this.inpValue = Number(event.target.value);
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,14 @@
-import {InjectionToken, NgModule} from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
-import { MatButtonModule } from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {reducers} from './store/redusers';
 
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    MatFormFieldModule,
     AppRoutingModule,
-    StoreModule.forRoot(reducers),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25,
-      logOnly: environment.production,
-    }),
-    EffectsModule.forRoot([]),
-    StoreRouterConnectingModule.forRoot(),
-    MatButtonModule,
-    BrowserAnimationsModule,
   ],
   providers: [
   ],
